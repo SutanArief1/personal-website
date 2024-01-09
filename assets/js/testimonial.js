0db529aaede708a5551681f5c3291804a37f44a9
Sutan Arief <sutanarief21@gmail.com>
2024-01-10 04:40:40 +0700

    Task day 8 done

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -1,77 +1,67 @@
-class Testimonial {
-  #image
-  #content
-  #author
-
-  constructor(image, content, author) {
-    this.image = image
-    this.content = content
-    this.author = author
+const testimonials = [
+  {
+    author: "Fajar",
+    content: "Terima Kasih Jasanya!",
+    image: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    rating: 4
+  },
+  {
+    author: "Natasha",
+    content: "Luar Biasa.",
+    image: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    rating: 5
+  },
+  {
+    author: "John Doe",
+    content: "Perlu ditingkatkan lagi..",
+    image: "https://images.pexels.com/photos/1182825/pexels-photo-1182825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    rating: 2
+  },
+  {
+    author: "Nolan",
+    content: "Sangat keren!!",
+    image: "https://images.pexels.com/photos/819530/pexels-photo-819530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    rating: 4
   }
+]
 
-  get image() {
-    return this.#image
-  }
+function renderAllTestimonial() {
+  let htmlTestimonial = ''
 
-  get content() {
-    return this.#content
-  }
-
-  get author() {
-    return this.#author
-  }
-
-  set image(value) {
-    this.#image = value
-  }
-
-  set content(value) {
-    this.#content = value
-  }
-
-  set author(value) {
-    this.#author = value
-  }
-
-  html() {
-    throw new Error('You must choose as author or company')
-  }
-}
-
-class PersonalTestimonial extends Testimonial {
-  html() {
-    return `
-    <div class="testimonial-card">
-        <img src="${this.image}" alt="testimonial" class="testimonial__profile-picture" />
-        <p class="testimonial__content">"${this.content}"</p>
-        <p class="testimonial__author">- ${this.author}</p>
+  testimonials.forEach((value) => {
+    const { image, content, author, rating } = value
+    htmlTestimonial += `
+      <div class="testimonial-card">
+        <img src="${image}" alt="testimonial" class="testimonial__profile-picture" />
+        <p class="testimonial__content">"${content}"</p>
+        <p class="testimonial__author">- ${author}</p>
+        <p class="testimonial__rating"><img src="assets/img/star.png" /> ${rating}</p>
       </div>
     `
-  }
+  })
+
+  document.getElementById('testimonials').innerHTML = htmlTestimonial
 }
 
-class CompanyTestimonial extends Testimonial {
-  html() {
-    return `
-    <div class="testimonial-card">
-        <img src="${this.image}" alt="testimonial" class="testimonial__profile-picture" />
-        <p class="testimonial__content">"${this.content}"</p>
-        <p class="testimonial__author">- ${this.author} Company</p>
-      </div>
-    `
-  }
+
+
+function filterTestimonial(rating) {
+  const filteredTestimonial = testimonials.filter((value) => value.rating === rating)
+  let htmlTestimonial = ''
+
+  filteredTestimonial.forEach((value) => {
+    const { image, content, author, rating } = value
+    htmlTestimonial += `
+        <div class="testimonial-card">
+          <img src="${image}" alt="testimonial" class="testimonial__profile-picture" />
+          <p class="testimonial__content">"${content}"</p>
+          <p class="testimonial__author">- ${author}</p>
+          <p class="testimonial__rating"><img src="assets/img/star.png" /> ${rating}</p>
+        </div>
+      `
+  })
+
+  document.getElementById('testimonials').innerHTML = htmlTestimonial
 }
 
-const testimonial1 = new PersonalTestimonial('https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'Terima Kasih Jasanya!', 'Fajar')
-const testimonial2 = new PersonalTestimonial('https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'Luar Biasa.', 'Natasha')
-const testimonial3 = new CompanyTestimonial('https://images.pexels.com/photos/1182825/pexels-photo-1182825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'Perlu ditingkatkan lagi..', 'John Doe')
-
-const testimonials = [testimonial1, testimonial2, testimonial3]
-
-htmlTestimonials = ''
-
-for (let i = 0; i < testimonials.length; i++) {
-  htmlTestimonials += testimonials[i].html()
-}
-
-document.getElementById('testimonials').innerHTML = htmlTestimonials
+renderAllTestimonial()
